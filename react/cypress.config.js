edd7dadc8dce5212abedfcf896995980eb792fd3
Nderi12 <nderikamau1212@gmail.com>
2024-09-12 00:39:48 +0300

    makinf base url dynamic

--- react/cypress.config.js
+++ react/cypress.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    // baseUrl: 'http://localhost:5173',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
     defaultCommandTimeout: 10000,
     screenshotOnRunFailure: true,
     video: false,
